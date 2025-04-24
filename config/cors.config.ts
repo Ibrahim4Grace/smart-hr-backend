d39a3b756cae975525e4059c932d42f1a3bfc55f
Akorede Agboola <akoredeagboola23@gmail.com>
2025-04-24 02:59:03 +0100

    feat: cors

---
+++ config/cors.config.ts
@@ -0,0 +1,19 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+export default () => {
+  const whitelist = (process.env.CORS_WHITELIST || '').split(',');
+
+  return {
+    cors: {
+      origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true,
+      optionsSuccessStatus: 201,
+    },
+  };
+};
