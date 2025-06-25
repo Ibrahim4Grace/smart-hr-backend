b1e3bc8a1e12ced495a4be3e615470d5b117ac65
Akorede Agboola <akoredeagboola23@gmail.com>
2025-06-25 16:45:23 +0100

    feat: payment features

---
+++ src/modules/pricing/interface/price.interface.ts
@@ -0,0 +1,19 @@
+export enum Region {
+    AFRICA = 'AFRICA',
+    INTERNATIONAL = 'INTERNATIONAL'
+}
+
+export enum AccessType {
+    HR_ONLY = 'HR_ONLY',
+    HR_AND_EMPLOYEE = 'HR_AND_EMPLOYEE'
+}
+
+export enum Duration {
+    MONTHLY = 'MONTHLY',
+    YEARLY = 'YEARLY'
+}
+
+export enum Currency {
+    USD = 'USD',
+    NGN = 'NGN'
+}
